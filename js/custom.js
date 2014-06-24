653f7c070b59eef35cb69bf0643d9ccbe1eef24d
Jonathan Perez <jonathan.phz@gmail.com>
2014-06-24 14:20:54 -0300

    Added Datepicker

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+
+$('.date').datepicker({
+    format: "yyyy/mm/dd",
+    startDate: "2012-01-01",
+    endDate: "2015-01-01",
+    todayBtn: "linked",
+    autoclose: true,
+    todayHighlight: true
+    });
